dd5678dfd18841b408a20e8c44b4fc6af028e750
Filipe Macedo <devmacedo@gmail.com>
2020-09-22 12:46:48 -0300

    feat: add users service

---
+++ src/services/users.service.ts
@@ -0,0 +1,49 @@
+import { AxiosResponse } from "axios";
+import api from "./api";
+
+export interface User {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  score: number;
+}
+
+export interface UsersResponse {
+  total_count: number;
+  incomplete_results: boolean;
+  items: User[];
+}
+
+/**
+ * This function is responsible 
+ * for get users
+ * @param query 
+ * @param perPage 
+ * @param page 
+ */
+export default async function getUsers(
+  query: string,
+  perPage = 10,
+  page = 0
+): Promise<UsersResponse> {
+  const { data }: AxiosResponse<UsersResponse> = await api.get(
+    `/users?q=${query}&per_page=${perPage}&page=${page}`
+  );
+
+  return data;
+}
