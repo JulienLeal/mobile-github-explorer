804b521c484f3b20ddf77d23ec67f95caa5d6ae4
Filipe Macedo <devmacedo@gmail.com>
2020-09-23 17:28:59 -0300

    fix: change type of png files

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,16 +1,13 @@
 
 declare module '*.svg' {
-  const content: string;
+  const content: any;
   export default content;
 }
 
-declare module '*.mp4' {
-  const content: string;
-  export default content;
-}
 
 declare module '*.png' {
-  const content: string;
+  const content: ImageSourcePropType;
+  
   export default content;
 }
 
