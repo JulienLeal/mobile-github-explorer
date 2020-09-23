4dae051ccf79f0ec1a9f300148b16ed849027674
Filipe Macedo <devmacedo@gmail.com>
2020-09-22 23:29:37 -0300

    feat: modify UserCard style (dark mode)

--- src/components/UserCard/UserCard.styles.tsx
+++ src/components/UserCard/UserCard.styles.tsx
@@ -2,16 +2,24 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   display: flex;
-  background: white;
-  border: 2px solid #eee;
-  border-radius: 5px;
+  background: red;
+  border-radius: 7px;
   flex: 1;
-  height: 100px;
+  max-height: 80px;
+  margin-bottom: 16px;
+  border-radius: 7px;
+  background: #222;
+  align-items: center;
+  flex-direction: row;
 `;
 
 export const UsernameText = styled.Text`
-  font-size: 16px;
-  color: #222;
+  font-size: 20px;
+  font-weight: normal;
+  line-height: 25px;
+  text-align: left;
+  color: #888;
+  font-family: "Nunito";
 `;
 
 export const VisitButton = styled.Button`
@@ -22,18 +30,9 @@ export const VisitButton = styled.Button`
   color: #222;
 `;
 
-export const DeleteButton = styled(VisitButton)`
-  background: #ff0000;
-  margin-right: 10px;
-  border: 2px solid transparent;
-  color: #fff;
+export const ProfileImage = styled.Image`
+  width: 60px;
+  height: 60px;
+  border-radius: ${60 / 2}px;
+  margin: 13px;
 `;
-
-export const ButtonsSection = styled.View`
-  display: flex;
-  flex: 1;
-  flex-direction: row;
-  justify-content: center;
-  align-items: flex-end;
-  margin-bottom: 10px;
-`
--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,41 +1,38 @@
 import React from "react";
 import { Alert, Linking } from "react-native";
 import { User } from "../../services/users.service";
-import {
-  Container,
-  UsernameText,
-  ButtonsSection,
-  DeleteButton,
-  VisitButton,
-} from "./UserCard.styles";
+import { Container, UsernameText, ProfileImage } from "./UserCard.styles";
 
 interface UserCardProps {
-  user: User;
-  onDelete(user: User): void;
+  // user: User;
+  // onDelete(user: User): void;
 }
 
-const UserCard: React.FC<UserCardProps> = ({ user, onDelete }) => {
+const UserCard: React.FC<UserCardProps> = () => {
   /**
    * This function is responsible
    * for open user profile
    */
-  async function handleVisitPress(): Promise<void> {
-    const isSupported = await Linking.canOpenURL(user.html_url);
+  // async function handleVisitPress(): Promise<void> {
+  //   const isSupported = await Linking.canOpenURL(user.html_url);
 
-    if (isSupported) {
-      await Linking.openURL(user.html_url);
-    } else {
-      Alert.alert("Não é possível abrir o link.");
-    }
-  }
+  //   if (isSupported) {
+  //     await Linking.openURL(user.html_url);
+  //   } else {
+  //     Alert.alert("Não é possível abrir o link.");
+  //   }
+  // }
 
   return (
     <Container>
-      <UsernameText>{user.login}</UsernameText>
-      <ButtonsSection>
-        <VisitButton title="Visitar perfil" onPress={handleVisitPress} />
-        <DeleteButton title="Remover" onPress={() => onDelete(user)} />
-      </ButtonsSection>
+      <ProfileImage
+        source={{ uri: "https://i.stack.imgur.com/8gLny.jpg?s=328&g=1" }}
+      />
+      <UsernameText>@filipemacedo</UsernameText>
+      {/* <ButtonsSection>
+        <VisitButton title="Visitar perfil" onPress={() => {}} />
+        <DeleteButton title="Remover" onPress={() => {}} />
+      </ButtonsSection> */}
     </Container>
   );
 };
